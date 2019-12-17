1c81d5be7d29581d4dbc196e07e5f5f9743933d2
Fankouzu <1046166@qq.com>
2019-12-17 19:16:22 +0800

    Update zombieAttack.sol

--- Solidity Lesson 04/contracts/zombieAttack.sol
+++ Solidity Lesson 04/contracts/zombieAttack.sol
@@ -2,32 +2,37 @@ pragma solidity ^0.5.12;
 
 import "./zombieHelper.sol";
 
-contract ZombieAttack is ZombieHelper {
-  uint randNonce = 0;
-  uint attackVictoryProbability = 70;
-
-  function randMod(uint _modulus) internal returns(uint) {
-    randNonce++;
-    return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
-  }
-  function setAttackVictoryProbability(uint _attackVictoryProbability)public onlyOwner{
-      attackVictoryProbability = _attackVictoryProbability;
-  }
-
-  function attack(uint _zombieId, uint _targetId) external onlyOwnerOf(_zombieId) {
-    require(msg.sender != zombieToOwner[_targetId],'The target zombie is yours!');
-    Zombie storage myZombie = zombies[_zombieId];
-    Zombie storage enemyZombie = zombies[_targetId];
-    uint rand = randMod(100);
-    if (rand <= attackVictoryProbability) {
-      myZombie.winCount++;
-      myZombie.level++;
-      enemyZombie.lossCount++;
-      multiply(_zombieId, enemyZombie.dna);
-    } else {
-      myZombie.lossCount++;
-      enemyZombie.winCount++;
-     _triggerCooldown(myZombie);
+contract ZombieAttack is ZombieHelper{
+    
+    uint randNonce = 0;
+    uint attackVictoryProbability = 70;
+    
+    function randMod(uint _modulus) internal returns(uint){
+        randNonce++;
+        return uint(keccak256(abi.encodePacked(now,msg.sender,randNonce))) % _modulus;
     }
-  }
+    
+    function setAttackVictoryProbability(uint _attackVictoryProbability)public onlyOwner{
+        attackVictoryProbability = _attackVictoryProbability;
+    }
+    
+    function attack(uint _zombieId,uint _targetId)external onlyOwnerOf(_zombieId) return(uint){
+        require(msg.sender != zombieToOwner[_targetId],'The target zombie is yours!');
+        Zombie storage myZombie = zombies[_zombieId];
+        Zombie storage enemyZombie = zombies[_targetId];
+        uint rand = randMod(100);
+        if(rand<=attackVictoryProbability){
+            myZombie.winCount++;
+            myZombie.level++;
+            enemyZombie.lossCount++;
+            multiply(_zombieId,enemyZombie.dna);
+            return _zombieId;
+        }else{
+            myZombie.lossCount++;
+            enemyZombie.winCount++;
+            _triggerCooldown(myZombie);
+            return _targetId;
+        }
+    }
+    
 }
