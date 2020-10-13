97e6a27ed7ca7278029a55d25698499dfc380302
崔棉大师 <1046166@qq.com>
2020-10-13 23:27:40 +0800

    Update erc20.sol

--- erc20.sol
+++ erc20.sol
@@ -1,7 +1,5 @@
 pragma solidity ^0.4.16;
 
-interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
-
 contract TokenERC20 {
     string public name;
     string public symbol;
